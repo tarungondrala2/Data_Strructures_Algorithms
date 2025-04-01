83b35c0c7fc50c12828599937de28c9fbf776ce2
Gondrala Tarun Srinivasulu <tarug1892@gmail.com>
2025-04-01 19:06:17 +0530

    Implemented delete operation using recursion on BST and a helper operation to find min value from the given node

--- src/Trees/BinarySearchTree.java
+++ src/Trees/BinarySearchTree.java
@@ -100,4 +100,49 @@ public class BinarySearchTree {
 		 
 		 return currentNode;
 	 }
+	 
+	 public void deleteNode(int value) {
+		 deleteNode(root, value);
+	 }
+	 
+	 /*
+	  * This node will delete a node from BST using recursion.
+	  */
+	 private Node deleteNode(Node currentNode, int value) {
+		 
+		 if(currentNode == null) return null;
+		 
+		 if(value < currentNode.value) {
+			 currentNode.left = deleteNode(currentNode.left, value);
+		 } else if(value > currentNode.value) {
+			 currentNode.right = deleteNode(currentNode.right, value);
+		 } else {
+			 if(currentNode.left == null && currentNode.right == null) {
+				 currentNode = null;
+			 } else if (currentNode.left == null) {
+				 currentNode = currentNode.right;
+			 } else if (currentNode.right == null) {
+				 currentNode = currentNode.left;
+			 } else {
+				 int subTreeMin = minValue(currentNode.right);
+				 currentNode.value = subTreeMin;
+				 currentNode.right = deleteNode(currentNode.right, subTreeMin);
+			 }
+		 }
+		 
+		 return currentNode;
+	 }
+	 
+	 /*
+	  * This method is a helper to determine minimum value in the BST.
+	  */
+	 private int minValue(Node currentNode) {
+		 
+		 while(currentNode.left != null) {
+			 currentNode = currentNode.left;
+		 }
+		 
+		 return currentNode.value;
+	 }
+	 
 }
