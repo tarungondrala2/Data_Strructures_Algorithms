de26d93c8ef08d14d6697c6b1a1c320eac8beecb
Gondrala Tarun Srinivasulu <tarug1892@gmail.com>
2025-04-04 20:43:53 +0530

    Implemented DFS Post order, Pre order, In order operation on a Binary Search Tree

--- src/Trees/BinarySearchTree.java
+++ src/Trees/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package Trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
 	
 	Node root;
@@ -146,6 +150,106 @@ public class BinarySearchTree {
 		 }
 		 
 		 return currentNode.value;
+	 }
+	 
+	 
+	 // <----------------- Tree Traversal operations --------------------------->
+	 
+	 /*
+	  * This method implements Breath First Search tree traversal operation;
+	  */
+	 public ArrayList<Integer> BFS() {
+		  
+		 Node currentNode = root;
+		 Queue<Node> queue = new LinkedList<>();
+		 ArrayList<Integer> results = new ArrayList<>();
+		 
+		 queue.add(currentNode);
+		 
+		 
+		 while(queue.size() > 0) {
+			 currentNode = queue.remove();
+			 results.add(currentNode.value);
+			 if(currentNode.left != null) {
+				 queue.add(currentNode.left);
+			 }
+			 if(currentNode.right != null) {
+				 queue.add(currentNode.right);
+			 }
+		 }
+		 return results;
+	 }
+	 
+	 /*
+	  * This method implements Depth First Search Pre order tree traversal operation using recursion
+	  */
+	 public ArrayList<Integer> DFSPreOrder() {
+		 
+		 ArrayList<Integer> results = new ArrayList<>();
+		 
+		 class Traverse {
+			 Traverse(Node currentNode) {
+				 results.add(currentNode.value);
+				 
+				 if(currentNode.left != null) {
+					 new Traverse(currentNode.left);
+				 }
+				 if(currentNode.right != null) {
+					 new Traverse(currentNode.right);
+				 }
+			 } 
+		 }
+		 
+		 new Traverse(root);
+		 return results;
+	 } 
+	 
+	 /*
+	  * This method implements Depth First Search Post order tree traversal operation using recursion.
+	  */
+	 public ArrayList<Integer> DFSPostOrder() {
+		 
+		 ArrayList<Integer> results = new ArrayList<>();
+		 
+		 class Traverse {
+			 
+			 Traverse(Node currentNode) {
+				 if(currentNode.left != null) {
+					 new Traverse(currentNode.left);
+				 }
+				 if(currentNode.right != null) {
+					 new Traverse(currentNode.right);
+				 }
+				 results.add(currentNode.value);
+			 }
+		 }
+		 
+		 new Traverse(root);
+		 return results;
+	 }
+	 
+	 public ArrayList<Integer> DFSInOrder() {
+		 
+		 ArrayList<Integer> results = new ArrayList<>();
+		 
+		 class Traverse {
+			 
+			 Traverse(Node currentNode) {
+				 
+				 if(currentNode.left != null) {
+					 new Traverse(currentNode.left);
+				 }
+				 
+				 results.add(currentNode.value);
+				 
+				 if(currentNode.right != null) {
+					 new Traverse(currentNode.right);
+				 }
+			 }
+		 }
+		 
+		 new Traverse(root);
+		 return results;
 	 }
 	 
 }
--- src/Trees/Main.java
+++ src/Trees/Main.java
@@ -15,21 +15,26 @@ public class Main {
 		myBST.insert(52);
 		myBST.insert(82);
 		
-		System.out.println("\nmyBST contains 27:");
-		System.out.println(myBST.rContains(27));
+//		System.out.println("\nmyBST contains 27:");
+//		System.out.println(myBST.rContains(27));
+//		
+//		System.out.println("\nmyBST contains 17:");
+//		System.out.println(myBST.rContains(17));
+//		
+//		BinarySearchTree rMyBST = new BinarySearchTree();
+//		
+//		rMyBST.rInsert(2);
+//		rMyBST.rInsert(1);
+//		rMyBST.rInsert(3);
+//		
+//		System.out.println("\nRoot: "+ rMyBST.root.value);
+//		System.out.println("\nRoot->Left: "+ rMyBST.root.left.value);
+//		System.out.println("\nRoot->Right: "+ rMyBST.root.right.value);
 		
-		System.out.println("\nmyBST contains 17:");
-		System.out.println(myBST.rContains(17));
-		
-		BinarySearchTree rMyBST = new BinarySearchTree();
-		
-		rMyBST.rInsert(2);
-		rMyBST.rInsert(1);
-		rMyBST.rInsert(3);
-		
-		System.out.println("\nRoot: "+ rMyBST.root.value);
-		System.out.println("\nRoot->Left: "+ rMyBST.root.left.value);
-		System.out.println("\nRoot->Right: "+ rMyBST.root.right.value);
+		System.out.println( myBST.BFS() );
+		System.out.println( myBST.DFSPreOrder() );
+		System.out.println( myBST.DFSPostOrder() );
+		System.out.println( myBST.DFSInOrder() );
 	}
 
 }
