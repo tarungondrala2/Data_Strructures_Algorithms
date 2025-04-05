b9aef08e2f09e70e22151da937ea152d5c3e1f6a
Gondrala Tarun Srinivasulu <tarug1892@gmail.com>
2025-04-05 22:06:54 +0530

    Implemented merge sort algorithm and a helper method 'merge" for merge sort algorithm

--- src/Algorithms/SortingAlgorithms.java
+++ src/Algorithms/SortingAlgorithms.java
@@ -15,16 +15,23 @@ public class SortingAlgorithms {
 		
 		int[] myArray2 = {4,2,6,5,1,3};
 		
-		selectionSort(myArray);
+		selectionSort(myArray2);
 		
-		System.out.println(Arrays.toString(myArray));
+		System.out.println(Arrays.toString(myArray2));
 		
 		
 		int[] myArray3 = {4,2,6,5,1,3};
 		
-		insertionSort(myArray);
+		insertionSort(myArray3);
 		
-		System.out.println(Arrays.toString(myArray));
+		System.out.println(Arrays.toString(myArray3));
+		
+		int[] myArray4 = {4,2,6,5,1,3};
+		
+		int[] mergeSortedArray = mergeSort(myArray);
+		
+		System.out.println(Arrays.toString(mergeSortedArray));
+		
 	}
 	
 	/*
@@ -90,6 +97,63 @@ public class SortingAlgorithms {
 			}
 					
 		}
+	}
+	
+	/*
+	 * THis method implements merge sort algorithm
+	 * 
+	 * 			Time complexity - O(n log n)
+	 * 			Breaking - O(log n), merging - O(n)			
+	 * 
+	 * 			Space complexity - O(
+	 */
+	public static int[] mergeSort(int[] array) {
+		
+		if(array.length == 1) return array;
+		
+		int midIndex = array.length/2;
+		int[] left = mergeSort(Arrays.copyOfRange(array,0,midIndex));
+		int[] right = mergeSort(Arrays.copyOfRange(array, midIndex, array.length));
+		
+		return merge(left, right);		
+	}
+	
+	/*
+	 * This method is helper for merger sort algorithm
+	 */
+	public static int[] merge(int[] array1, int[] array2) {
+		int[] combined = new int[array1.length + array2.length];
+		
+		int index = 0;
+		int i = 0;
+		int j = 0;
+		
+		while(i < array1.length && j < array2.length) {
+			
+			if(array1[i] < array2[j]) {
+				combined[index] = array1[i];
+				index++;
+				i++;
+			} else {
+				combined[index] = array2[j];
+				index++;
+				j++;
+			}
+		}
+		
+		while(i < array1.length) {
+			combined[index] = array1[i];
+			index++;
+			i++;
+		}
+		
+		while(j < array2.length) {
+			combined[index] = array2[j];
+			index++;
+			j++;
+		}
+		
+		return combined;
 	}
 
 }
