e2a7ec705589c6c8212b9db8b373a06cf879cc39
Gondrala Tarun Srinivasulu <tarug1892@gmail.com>
2025-04-13 21:07:57 +0530

    Implemented logic for length of longest palindrome sub string

--- src/ProblemSolving/Main.java
+++ src/ProblemSolving/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println(lengthOfLongestPalindrome("babad"));
 		
 	}
 
@@ -15,5 +16,42 @@ public class Main {
 //		
 //	}
 	
+	public static int lengthOfLongestPalindrome(String string) {
+		
+		String longestPalindrome = "";
+		String palindrome = "";
+		
+		for(int i = 1; i < string.length(); i++) {
+			
+			int left = i;
+			int right = i;
+			
+			palindrome = getPalindrome(string, left, right);
+			
+			if(palindrome.length() > longestPalindrome.length()) longestPalindrome = palindrome;
+			
+			left = i-1;
+			right = i;
+			
+			palindrome = getPalindrome(string, left, right);
+			
+			if(palindrome.length() > longestPalindrome.length()) longestPalindrome = palindrome;
+				
+		}
+		
+		return longestPalindrome.length();
+	}
+	
+	public static String getPalindrome(String string, int left, int right) {
+		
+		while(string.charAt(left) == string.charAt(right)) {
+			left--;
+			right++;
+			
+			if(left == -1 || right == string.length()) break;
+		}
+		
+		return string.substring(left+1,right);
+	}
 
 }
