09a573d471216fdc95897e361570b5ecf17659c9
Gondrala Tarun Srinivasulu <tgondrala@opentext.com>
2025-04-15 20:48:33 +0530

    Implemented a problem solving statement 'reversing a array by k"

--- src/ProblemSolving/Main.java
+++ src/ProblemSolving/Main.java
@@ -7,14 +7,37 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(lengthOfLongestPalindrome("babad"));
+		//System.out.println(lengthOfLongestPalindrome("babad"));
 		
+		int[] array = {1,2,3,4,5,6};
+		array = reverseArrayByK(array,4);
+		System.out.println(Arrays.toString(array));
 	}
 
 //	public static int findLongestPalindromeLength(String string) {
 //		
 //		
 //	}
+	
+	public static int[] reverseArrayByK(int[] array, int k) {
+	    List<Integer> myList = new ArrayList<>();
+	    for (int num : array) {
+	        myList.add(num);
+	    }
+
+	    for (int i = 1; i <= k; i++) {
+	        int temp = myList.remove(myList.size() - 1);
+	        myList.add(0, temp);
+	    }
+
+	    int[] result = new int[myList.size()];
+	    for (int i = 0; i < myList.size(); i++) {
+	        result[i] = myList.get(i);
+	    }
+
+	    return result;
+	}
+
 	
 	public static int lengthOfLongestPalindrome(String string) {
 		
